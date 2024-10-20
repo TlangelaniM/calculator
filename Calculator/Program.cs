1de758be1d712e195169e797f2b476155920a99b
Clang <clangelanim@hotmail.com>
2024-10-20 15:31:46 +0200

    added addition method

---
+++ Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+Console.WriteLine("enter num1");
+int numb1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("enter num2");
+int numb2 = int.Parse(Console.ReadLine());
+
+var total = Add(numb1, numb2);
+Console.WriteLine("The Total is: " + total);
+
+static int Add(int number1, int number2)
+{
+    var total = number1 + number2;
+    return total;
+}
